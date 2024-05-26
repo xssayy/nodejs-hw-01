9ae6d3b4c83382eb3abce9965d5142fd6a729c91
Mykyta Kudlai <nikiatanik@gmail.com>
2024-05-26 20:41:13 +0200

    fixed

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,4 +1,5 @@
 import path from 'node:path';
 const pathToWorkDir = path.join(process.cwd());
 
-export const PATH_DB = path.join(pathToWorkDir, '/src/db/db.json');
+export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
+console.log(PATH_DB);
